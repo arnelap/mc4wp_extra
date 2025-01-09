c5753945b0219e6220403b03f14f6e225e911be8
Arne Lap <arnelap@users.noreply.github.com>
2025-01-09 15:39:08 +0700

    init

---
+++ README.txt
@@ -0,0 +1,43 @@
+=== MC4WP Extra ===
+Contributors: lapzor
+Donate link: https://lapzor.com/
+Tags: comments, spam
+Requires Plugins: mailchimp-for-wp
+Requires at least: 6.5
+Tested up to: 6.5
+Stable tag: 6.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds extra functionality to the Mailchimp for Wordpress plugin.
+This plugin is created by a third party, not by Mailchimp for Wordpress, ibericode or Mailchimp. 
+
+== Description ==
+
+This is the long description.  No limit, and you can use Markdown (as well as in the following sections).
+
+
+
+== Frequently Asked Questions ==
+
+= A question that someone might have =
+
+An answer to that question.
+
+= What about foo bar? =
+
+Answer to foo bar dilemma.
+
+== Screenshots ==
+
+1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
+the /assets directory or the directory that contains the stable readme.txt (tags or trunk). Screenshots in the /assets
+directory take precedence. For example, `/assets/screenshot-1.png` would win over `/tags/4.3/screenshot-1.png`
+(or jpg, jpeg, gif).
+2. This is the second screen shot
+
+== Changelog ==
+
+= 1.0 =
+* A change since the previous version.
+* Another change.
---
+++ mc4wp-extra.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ *
+ * @link              https://lapzor.com
+ * @since             1.0.0
+ * @package           Mc4wp_Extra
+ *
+ * @wordpress-plugin
+ * Plugin Name:       MC4WP Extra
+ * Requires Plugins:  mailchimp-for-wordpress
+ * Plugin URI:        https://lapzor.com
+ * Description:       Extra features for MC4WP. 
+ * Version:           1.0.0
+ * Author:            Lapzor
+ * Author URI:        https://lapzor.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       mc4wp-extra
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+define( 'MC4WP_EXTRA_VERSION', '1.0.0' );
+
+class mc4wp_extra {
+
+	public function __construct() {
+		add_action('admin_init', array($this, 'mc4wp_extra_register_settings'));
+
+		add_action('mc4wp_admin_after_integration_settings', array($this, 'mc4wp_extra_admin_after_integration_settings'), 40, 2);
+
+		$this->mc4wp_extra_hooks();
+	}
+
+	public function mc4wp_extra_register_settings() {
+    	register_setting('mc4wp_integrations_settings', 'mc4wp_extra_woocommerce_tag');
+	}
+
+	public function mc4wp_extra_admin_after_integration_settings(MC4WP_Integration $integration, $opts)
+	{
+		$file = __DIR__ . '/' . $integration->slug . '.php';
+		if (file_exists($file)) {
+			include $file;
+		}
+	}
+
+	public function mc4wp_extra_hooks() {
+
+		if (null !== get_option('mc4wp_extra_woocommerce_tag') && !empty(get_option('mc4wp_extra_woocommerce_tag'))) {
+		add_filter( 'mc4wp_integration_woocommerce_subscriber_data', function(MC4WP_MailChimp_Subscriber $subscriber) {
+			$tags = explode(',', get_option('mc4wp_extra_woocommerce_tag'));
+			foreach($tags as $tag) {
+				$subscriber->tags[] = trim($tag);
+			}
+			return $subscriber;
+		 });
+		}
+	}
+
+}
+new mc4wp_extra();
+
+
+
+
